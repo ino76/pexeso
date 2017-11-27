01224c63e650bb58c63fd511db4861d2d3e6151d
Daniel Cech <daniel8cech@gmail.com>
2017-11-27 19:28:12 +0100

    added back image 'pexeso'

--- js/script.js
+++ js/script.js
@@ -7,6 +7,7 @@ const cards = document.querySelectorAll('.card')
 let numberOfFlipped = 0
 let listOfFlipped = []
 let last
+let inAnimation = false
 
 cards.forEach(function(c){
     c.addEventListener('click', flipManager)
@@ -14,11 +15,18 @@ cards.forEach(function(c){
 
 
 function flipManager(c) {
-    if(numberOfFlipped < 2 && this != last) {
+    if( numberOfFlipped < 2 && this != last) {
+        if(inAnimation) {
+            return
+        }
+        inAnimation = true
+        last = this
         numberOfFlipped++
         listOfFlipped.push(this)
         flip(this)
-        last = this
+        setTimeout(function(){
+            inAnimation = false
+        }, 500)
     }
 
     if(numberOfFlipped == 2) {
@@ -27,6 +35,10 @@ function flipManager(c) {
             last = null
             numberOfFlipped = 0
             listOfFlipped = []
+            inAnimation = true
+            setTimeout(function(){
+                inAnimation = false
+            }, 800)
         }, 1500)
     }
     console.log(numberOfFlipped)
