6dbaa9760ee37284d97d2aab05216b77e9a03041
Daniel Cech <daniel8cech@gmail.com>
2017-11-27 15:50:31 +0100

    implemented cards and bacic flipping logic/script

--- js/script.js
+++ js/script.js
@@ -1,3 +1,38 @@
-// Pexeso
+// *** title ***
 // author: Daniel Cech
 // date: Nov 23, 2017
+
+
+const cards = document.querySelectorAll('.card')
+let numberOfFlipped = 0
+let listOfFlipped = []
+let last
+
+cards.forEach(function(c){
+    c.addEventListener('click', flipManager)
+})
+
+
+function flipManager(c) {
+    if(numberOfFlipped < 2 && this != last) {
+        numberOfFlipped++
+        listOfFlipped.push(this)
+        flip(this)
+        last = this
+    }
+
+    if(numberOfFlipped == 2) {
+        setTimeout(function(){
+            listOfFlipped.forEach(c => flip(c))
+            last = null
+            numberOfFlipped = 0
+            listOfFlipped = []
+        }, 1500)
+    }
+    console.log(numberOfFlipped)
+}
+
+
+function flip(c) {
+    c.classList.toggle('flipped')
+}
