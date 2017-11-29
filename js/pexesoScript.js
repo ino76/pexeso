5f61b7b1c37545f8db7fb584a0a8e0317afd6697
Daniel Cech <daniel8cech@gmail.com>
2017-11-29 19:35:05 +0100

    edited images of langs and (resized to 150x150 and made transparent background). Added shuffle function so now langs are always shuffled differently.

--- js/pexesoScript.js
+++ js/pexesoScript.js
@@ -3,15 +3,68 @@
 // date: Nov 23, 2017
 
 
-const cards = document.querySelectorAll('.card')
+let listOfLangs = [   "bash",
+                        "c",
+                        "cpp",
+                        "crystal",
+                        "csharp",
+                        "elixir",
+                        "git",
+                        "go",
+                        "haskell",
+                        "java",
+                        "javascript",
+                        "kotlin",
+                        "python",
+                        "ruby"
+                    ]
+
+
+// every element should be twice in array
+listOfLangs.push.apply(listOfLangs, listOfLangs)
+
+const game = document.getElementById('game')
 let numberOfFlipped = 0
 let listOfFlipped = []
 let last
 let inAnimation = false
+let lastLang
 
-cards.forEach(function(c){
-    c.addEventListener('click', flipManager)
-})
+
+function start(s){
+    shuffle(listOfLangs)
+    listOfLangs.forEach(x => {
+        game.innerHTML += `<section class="container">
+        <div class="card">
+                <figure class="front"><span></span></figure>
+                <figure class="back"><img src="images/logos/${x}.png" data-lang="${x}"></figure>
+        </div>
+    </section>`
+    })
+    const cards = document.querySelectorAll('.card')
+    cards.forEach(function(c){
+        c.addEventListener('click', flipManager)
+    })
+}
+
+
+function shuffle(array) {
+    var m = array.length, t, i;
+  
+    // While there remain elements to shuffle…
+    while (m) {
+  
+      // Pick a remaining element…
+      i = Math.floor(Math.random() * m--);
+  
+      // And swap it with the current element.
+      t = array[m];
+      array[m] = array[i];
+      array[i] = t;
+    }
+  
+    return array;
+}
 
 
 function flipManager(c) {
